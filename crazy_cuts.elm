225745142e54388be00e18c3a518fdc566e0d678
Adi Shavit <adishavit@gmail.com>
2015-12-31 17:21:42 +0200

    Added twin.

--- crazy_cuts.elm
+++ crazy_cuts.elm
@@ -3,81 +3,121 @@ import Color exposing (..)
 import Graphics.Collage exposing (..)
 import Graphics.Element exposing (..)
 import Random exposing (float, generate, initialSeed)
+import Array
+import Maybe exposing (withDefault)
+import Debug
 
 main : Element
 main =
---   show <| flatten <| toListOfPairs <| appendFst <| crazyQuad  
+    let 
+        poly = toPolygon crazyQuad
+        twin = toPolygon (genTwin crazyQuad)
+    in
+   --show <| genCrazyQuad 42 
+   --show <|  crazyQuad
+   --show <| poly  
 --{-
-  collage 400 400
-    [ crazyPoly
-      |> outlined defaultLine
-        --|> filled blue
-        --|> move (-10,0)
-        --|> scale 20
-    ]
+    collage 400 400
+        [ poly |> filled red
+      --  , poly |> outlined defaultLine
+        , twin |> filled red
+      --  , twin |> outlined defaultLine
+        ]
 --}
 -- convert to HTML Polygon
-crazyPoly = crazyQuad 
-         |> flatten
-         |> scalePts 100 
-         |> polygon
+toPolygon poly = poly  
+                |> flatten
+                |> scalePath 100 
+                |> polygon
 
 
-crazyQuad = genCrazyQuad 42 |> toListOfPairs
+crazyQuad = toPoly <| genCrazyQuad 42
 
 
 genCrazyQuad seed = 
     let seed0 = initialSeed seed
         (x0,seed1) = generate (float -1 0) seed0
-        (y0,seed2) = generate (float  0 1) seed1
+        (y0,seed2) = generate (float  0 0.5) seed1
         (x1,seed3) = generate (float  0 1) seed2
-        (y1,seed4) = generate (float  0 1) seed3
-        length = norm ((x1,x2) `minus` (x0,y0))
+        (y1,seed4) = generate (float  0 0.5) seed3
+        length = norm ((x1,y1) `minus` (x0,y0))
         (x2,seed5) = generate (float  0 1) seed4
-        (y2,seed6) = generate (float -1 0) seed5
+        (y2,seed6) = generate (float -0.5 0) seed5
         (x3,y3)    = (x2 - length, y2)        
     in 
         --[ (-1,1), (1,1), (1,-1), (-1,-1) ]
-        [(x0,y0), (x1,y1), (x2,y2), (x3,y3)]
+        [(x0,y0), (x1,y1), (x2,y2), (x3,y3)]  
+
+genTwin poly =
+    let 
+        getLegStartPoint ind pp = 
+            let
+                dummy = (0,0)
+                polyArray = Array.fromList pp                
+                leg0 = withDefault [] <| Array.get ind polyArray                
+            in
+                withDefault dummy <| head leg0
+              
+        getOffset1 pp = neg <| getLegStartPoint 0 pp          
+        getAngle pp = 
+            let (x1,y1) = getLegStartPoint 1 pp 
+            in -(atan2 y1 x1)       
+
+        getOffset2 pp = getLegStartPoint 3 pp 
+        
+        movedPoly = poly |> movePoly (getOffset1 poly)
+        rotPoly = movedPoly |> rotatePoly (getAngle movedPoly) 
+        movedPoly2 = movePoly (getOffset2 poly) rotPoly
+    in
+        movedPoly2
+
         
 
+
 -- transformation functions
--- type alias Point = (Float, Float)
+type alias Point = (Float, Float)
+neg (x,y) = (-x,-y)
 scale s (x,y) = (s*x, s*y)
 move (dx,dy) (x,y) = (x + dx, y + dy)
 rotate rads (x,y) = (cos(rads)*x - sin(rads)*y, sin(rads)*x + cos(rads)*y)
 plus (x0,y0) (x1,y1) = (x0+x1, y0+y1)
 minus (x0,y0) (x1,y1) = (x0-x1, y0-y1)
-norm (x,y) = sqrt (x*x+y*y)
+norm (x,y) = sqrt(x*x+y*y)
 normalize pt = scale (1.0 / norm pt) pt
 normal (x,y) = normalize (-y, x)
 
+type alias Path = List Point
+scalePath s pts = map (scale s) pts
+movePath offset pts = map (move offset) pts
+rotatePath angle pts = map (rotate angle) pts
 
-{-
-toCrazyQuad : List Point -> List List a 
-toCrazyQuad ptListQuad = 
-    let
-        withFirstAsLast : List Point
-        withFirstAsLast = ptListQuad ++ [head ptListQuad]  -- add head to back of list
-    in
-        case ptListQuad of
-            [] -> []
-            head::tail -> []
--}
- 
+type alias Poly = List Path
+movePoly offset poly = map (movePath offset) poly
+rotatePoly angle poly = map (rotatePath angle) poly
+
+
+
+-- utils
 appendFst lst = 
     case lst of 
         head::tail -> lst ++ [ head ]
         [] -> []
 
 
-toListOfPairs lst = 
+toPoly_rec lst = 
     case lst of
         [] -> []
         head::tail -> 
             case tail of 
                 [] -> []
-                nxt::rest -> [head, nxt] :: toListOfPairs tail
+                nxt::rest -> [head, nxt] :: toPoly_rec tail
+
+toPoly lst = 
+    let 
+        first = withDefault (0,0) (head lst)
+        last = withDefault (0,0) (head (reverse lst))
+    in
+        toPoly_rec lst ++ [[last, first]]
 
 
 flatten lst =
@@ -85,4 +125,3 @@ flatten lst =
         [] -> []
         head::tail -> head ++ flatten tail    
             
-scalePts s pts = map (scale s) pts
